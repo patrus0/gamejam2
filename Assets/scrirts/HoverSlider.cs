0fe7357b94be09f311ccbc0e2bb4f06dd5049f51
patrus0 <patrushev2006inbox.ru@gmail.com>
2026-04-24 21:46:50 +0300

    add notebook

---
+++ Assets/scrirts/HoverSlider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverSlide : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public RectTransform panel; // Объект, который двигаем
+    public Vector2 hiddenPos;   // Координаты за экраном (например, 0, -200)
+    public Vector2 visiblePos;  // Координаты при наведении (например, 0, 0)
+    public float speed = 10f;
+
+    private Vector2 targetPos;
+
+    void Start()
+    {
+        targetPos = hiddenPos;
+        panel.anchoredPosition = hiddenPos;
+    }
+
+    void Update()
+    {
+        // Плавное движение к цели
+        panel.anchoredPosition = Vector2.Lerp(panel.anchoredPosition, targetPos, Time.deltaTime * speed);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        targetPos = visiblePos;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        targetPos = hiddenPos;
+    }
+}
